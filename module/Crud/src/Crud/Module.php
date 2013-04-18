cc86d5423a963110490abab579d9e519d2985774
Carlos A. Ferrari <caferrari@gmail.com>
2013-04-18 14:29:22 -0300

    Adicionado modulo de crud abstrato

---
+++ module/Crud/src/Crud/Module.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Crud;
+
+class Module
+{
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                     __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return array();
+    }
+
+    public function getServiceConfig()
+    {
+        return array();
+    }
+
+}
