b21d7cad266c5f6c035d487ed397eb1ec93a03b7
Carlos André Ferrari <caferrari@gmail.com>
2013-05-22 09:47:32 -0300

    Adicionada a entidade Endereco

---
+++ module/Base/src/Base/Entity/Endereco.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Base\Entity;
+
+use Doctrine\ORM\Mapping as ORM,
+    Crud\AbstractEntity,
+    Zend\Crypt\Password\Bcrypt;
+
+/**
+ * @ORM\Table(name="endereco")
+ * @ORM\Entity(repositoryClass="Base\Repository\Endereco") @ORM\HasLifecycleCallbacks
+ */
+class Endereco extends AbstractEntity
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Base\Entity\Cidade", inversedBy="cidade")
+     */
+    protected $cidade;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    protected $logradouro;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $complemento;
+
+    /**
+     * @ORM\Column(type="string", length=9)
+     */
+    protected $cep;
+
+}
