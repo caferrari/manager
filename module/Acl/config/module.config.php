234192aaf5462ef6ff906b02740760d831306390
Carlos André Ferrari <caferrari@gmail.com>
2013-06-03 15:28:49 -0300

    Limpando modulo

--- module/Acl/config/module.config.php
+++ module/Acl/config/module.config.php
@@ -20,51 +20,9 @@ return array(
     'data-fixture' => array(
         'Acl_fixture' => __DIR__ . '/../src/Acl/Fixture',
     ),
-    'router' => array(
-        'routes' => array(
-            'acl-crud' => array(
-                'type' => 'Segment',
-                'options' => array(
-                    'route' => 'acl/:controller[/:action][?id=:id][?uf=:uf]',
-                    'defaults' => array(
-                        'controller' => 'roles',
-                        'action' => 'index',
-                        'id' => null,
-                        'uf' => null
-                    ),
-                    'constraints' => array(
-                        'id' => '[0-9]+',
-                        'uf' => '[A-Z]{2}'
-                    )
-                ),
-            )
-        )
-    ),
     'controllers' => array(
         'invokables' => array(
-            'roles' => 'Acl\Controller\Roles'
+            'role' => 'Acl\Controller\Role'
         ),
-    ),
-    'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-        'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ),
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
-    'view_helpers' => array(
-        'invokables' => array(
-            'bootstrapRow' => 'Common\Helper\BootstrapRow',
-            'FlashMessages' => 'Common\View\Helper\FlashMessages',
-        )
     )
 );
